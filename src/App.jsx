de8b7cdf30c351d43bd3397f95000253ad0eb9b5
preeti223 <sthapriti897@gmail.com>
2024-10-20 22:34:36 -0800

    Completd the Project member profile page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,32 @@ import Sidebar from "./Sidebar";
 import Header from "./Header";
 import Home from "./Home";
 import UpcomingTasks from './upcomingtasks';
-
+import TeamMember from './TeamMember';
 
 function App() {
-
+  const teamMembers = [
+    {
+      role: 'Team Manager',
+      name: 'John Doe',
+      experience: '10',
+      projects: 'Project A, Project B',
+      email: 'john.doe@example.com',
+    },
+    {
+      role: 'Developer',
+      name: 'Alice Smith',
+      experience: '5',
+      projects: 'Project A',
+      email: 'alice.smith@example.com',
+    },
+    {
+      role: 'Developer',
+      name: 'Bob Johnson',
+      experience: '4',
+      projects: 'Project B',
+      email: 'bob.johnson@example.com',
+    },
+  ];
 
   return (
     <>
@@ -16,7 +38,7 @@ function App() {
      <Header/>
      <Home/>
      <UpcomingTasks/>
-     
+     <TeamMember/>
      </div>
      </div>
     </>
---
+++ src/TeamMember.jsx
@@ -0,0 +1,43 @@
+
+import React from 'react';
+
+
+const TeamMember = ({ role, name, experience, projects, email }) => {
+  return (
+    <div className="App">
+         <h1>Project Name: {}</h1>
+         <h2>Team Members</h2>
+
+         <div className='member-card'>
+
+      <div className="team-member-card">
+      <div className="team-member-image">
+        <img src="https://via.placeholder.com/150" alt="Profile" />
+      </div>
+      <h3>Team Member{role}</h3>
+      <p><strong>Name:</strong> {name}</p>
+      <p><strong>Experience:</strong> {experience} years</p>
+      <p><strong>Working Projects:</strong> {projects}</p>
+      <p><strong>Email:</strong> {email}</p>
+    </div>
+
+    <div className="team-member-card">
+      <div className="team-member-image">
+        <img src="https://via.placeholder.com/150" alt="Profile" />
+      </div>
+      <h3>Team Member{role}</h3>
+      <p><strong>Name:</strong> {name}</p>
+      <p><strong>Experience:</strong> {experience} years</p>
+      <p><strong>Working Projects:</strong> {projects}</p>
+      <p><strong>Email:</strong> {email}</p>
+    </div>
+
+    </div>
+
+    </div>
+
+    
+  );
+};
+
+export default TeamMember;
